6a6dacdc5c4b5c0b4230bd07014b427c5683a0a0
Stormbreakerr20 <luvsharma.del@gmail.com>
2025-04-25 23:46:15 +0530

    Neural Net Added

---
+++ frontend/visualisation-tool/app/playground/components/DL.tsx
@@ -0,0 +1,12 @@
+// pages/external.tsx
+
+const DL = () => {
+  return (
+    <iframe
+      src="http://127.0.0.1:5501/frontend/visualisation-tool/public/index.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  );
+};
+
+export default DL;
